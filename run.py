af886867eeab9d20f7a9f35df5cb5ad56affca54
Joao4569 <johnnyrod4569@gmail.com>
2022-02-09 10:20:41 +0000

    chore: Add comments to code taken from code institute love sandwiches walkthrough project

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+""" All code below up until line *** was sourced from Code Institutes "Love Sandwiches Walkthrough Project",
+"Getting Set Up" course videos"""
+
+# Import gspread library in order to access and update data on spreadsheet
 import gspread
+
+# Import Credentials class from google-auth library, service account function in order to set up authentication needed to access Google Cloud
 from google.oauth2.service_account import Credentials
+
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Scope constant for IAM
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
